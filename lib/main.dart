a4757430964ec8f317b608784d8283db6f0c0d4c
DuDeel <hopkalissa@gmail.com>
2023-10-21 19:34:45 +0400

    init clear project

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'CalorieCounter',
+      theme: ThemeData(
+        primaryColor: const Color(0xFF8F6BF2),
+        scaffoldBackgroundColor: const Color(0xFFEFF1F3),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
